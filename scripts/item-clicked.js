735d5fa01db5f7a8886d29ab9a9c2f78a0e86ffe
Kemi Chris <ekeminichristopher123@gmail.com>
2023-06-29 06:08:41 +0100

    item clicked script

--- scripts/item-clicked.js
+++ scripts/item-clicked.js
@@ -16,3 +16,42 @@ closeNav.addEventListener("click", ()=> {
     closeNav.classList.add("inactive");
     openNav.classList.remove("inactive");
 });
+
+
+/* ////// IMAGE SECTIONS ////// */
+const imageView = document.querySelectorAll(".img-sec");
+const infoImage = document.querySelector(".info-image");
+
+let imgSrc0 = imageView[0].src
+let imgSrc1 = imageView[1].src
+let imgSrc2 = imageView[2].src
+let imgSrc3 = imageView[3].src
+
+imageView[0].addEventListener("click", ()=> {
+    imageView[0].classList.add("active")
+    imageView[1].classList.remove("active")
+    imageView[2].classList.remove("active")
+    imageView[3].classList.remove("active")
+    infoImage.src = imgSrc0;
+});
+imageView[1].addEventListener("click", ()=> {
+    imageView[1].classList.add("active")
+    imageView[0].classList.remove("active")
+    imageView[2].classList.remove("active")
+    imageView[3].classList.remove("active")
+    infoImage.src = imgSrc1;
+});
+imageView[2].addEventListener("click", ()=> {
+    imageView[2].classList.add("active")
+    imageView[1].classList.remove("active")
+    imageView[0].classList.remove("active")
+    imageView[3].classList.remove("active")
+    infoImage.src = imgSrc2;
+});
+imageView[3].addEventListener("click", ()=> {
+    imageView[3].classList.add("active")
+    imageView[1].classList.remove("active")
+    imageView[2].classList.remove("active")
+    imageView[0].classList.remove("active")
+    infoImage.src = imgSrc3;
+});
