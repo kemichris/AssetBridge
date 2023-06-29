a0a0b5585581eaee3438a7229ca9c91424de3964
Kemi Chris <ekeminichristopher123@gmail.com>
2023-06-29 04:03:05 +0100

    create token responsive

--- scripts/create-token.js
+++ scripts/create-token.js
@@ -1,5 +1,26 @@
-/* ////// FORM VALIDATION ////// */
+/* ////// NAV BAR MENU DROPDOWN ////// */
+const openNav = document.getElementById("open-nav");
+const closeNav = document.getElementById("close-nav");
+const sideBarMenu = document.querySelector(".sidebar-menu")
 
+// opening the nav menu 
+openNav.addEventListener("click", ()=> {
+    sideBarMenu.classList.add("active");
+    openNav.classList.add("inactive");
+    closeNav.classList.remove("inactive");
+});
+
+// closing the nav menu 
+closeNav.addEventListener("click", ()=> {
+    sideBarMenu.classList.remove("active");
+    closeNav.classList.add("inactive");
+    openNav.classList.remove("inactive");
+});
+
+
+
+
+/* ////// FORM VALIDATION ////// */
 // anchor tags 
 const nextBtn = document.querySelector(".next-btn");
 const page2Next = document.querySelector(".page-2-next");
